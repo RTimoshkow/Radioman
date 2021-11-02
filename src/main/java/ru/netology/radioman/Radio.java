1a84bec5e76f4cb832c6a34d6713bc34782f7ce0
Timoshkov Roman <timoshkow@mail.ru>
2021-11-02 20:11:35 +0300

    methods and tests

--- src/main/java/ru/netology/radioman/Radio.java
+++ src/main/java/ru/netology/radioman/Radio.java
@@ -1,4 +1,72 @@
 package ru.netology.radioman;
 
 public class Radio {
+    private int userStation;
+    private int currentStation;
+    private int volume;
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setNextStation(int newCurrentStation) {
+        if (newCurrentStation < 9) {
+            currentStation = newCurrentStation + 1;
+        }
+        if (newCurrentStation == 9 || newCurrentStation < 0 || newCurrentStation > 9) {
+            currentStation = 0;
+        }
+    }
+
+    public void setPrevStation(int newCurrentStation) {
+        if (newCurrentStation <= 9 && newCurrentStation > 0) {
+            currentStation = newCurrentStation - 1;
+        }
+        if (newCurrentStation == 0) {
+            currentStation = 9;
+        }
+        if (newCurrentStation < 0) {
+            currentStation = 0;
+        }
+    }
+
+    public int getUserStation() {
+        return userStation;
+    }
+
+    public void setUserStation(int userInstallation) {
+        if (userInstallation >= 0 && userInstallation <= 9) {
+            userStation = userInstallation;
+        } else {
+            userStation = 0;
+        }
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setIncreaseVolume(int newVolume) {
+        if (newVolume < 10){
+            volume = newVolume + 1;
+        }
+        if (newVolume == 10){
+            volume = newVolume;
+        }
+        if (newVolume > 10 || newVolume < 0){
+            volume = 0;
+        }
+    }
+
+    public void setLowVolume(int newVolume){
+        if (newVolume <= 10){
+            volume = newVolume - 1;
+        }
+        if (newVolume == 0){
+            volume = newVolume;
+        }
+        if (newVolume > 10 || newVolume < 0){
+            volume = 0;
+        }
+    }
 }
--- src/test/java/ru/netology/radioman/RadioTest.java
+++ src/test/java/ru/netology/radioman/RadioTest.java
@@ -1,8 +1,66 @@
 package ru.netology.radioman;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-class RadioTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class RadioTest {
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/nextStation.csv")
+    public void shouldNextStation(String test, int newCurrentStation, int expected) {
+        Radio radio = new Radio();
+
+        radio.setNextStation(newCurrentStation);
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/prevStation.csv")
+    public void shouldPrevStation(String test, int newCurrentStation, int expected) {
+        Radio radio = new Radio();
+
+        radio.setPrevStation(newCurrentStation);
+        int actual = radio.getCurrentStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/userChoise.csv")
+    public void shouldUserStation(String test, int userInstallation, int expected) {
+        Radio radio = new Radio();
+
+        radio.setUserStation(userInstallation);
+        int actual = radio.getUserStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/increaseVolume.csv")
+    public void shouldIncreaseVolume(String test, int newVolume, int expected){
+        Radio radio = new Radio();
+
+        radio.setIncreaseVolume(newVolume);
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/lowVolume.csv")
+    public void shouldLowVolume(String test, int newVolume, int expected){
+        Radio radio = new Radio();
+
+        radio.setLowVolume(newVolume);
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
 
 }
